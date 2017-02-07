0db707f9253384132b3e267158784d7bf5898ad5
satodd <saatodd@gmail.com>
2017-02-07 15:19:37 -0700

    Moved button to own class

--- jsx/Page.jsx
+++ jsx/Page.jsx
@@ -1,29 +1,39 @@
 var React = require('react')
 var Header = require('./Header.jsx')
 
-module.exports = React.createClass({
-  'displayName': 'BlankPage.jsx',
-	getInitialState: function(){
-	    return {
-	        isLiked: false,
+var LikeButton = React.createClass({
+  getInitialState: function(){
+      return {
+          isLiked: false,
           text: ":|"
-	    };
-	},
+      };
+  },
 
   likeThis: function() {
-  	if (!this.state.isLiked) {
-  		this.setState({isLiked: true, text:":)"})
-  	}
+    if (!this.state.isLiked) {
+      this.setState({
+        isLiked: true,
+        text:":)"
+      });
+    }
   },
+
+  render: function() {
+    return <button type="button" id="likeButton" onClick={this.likeThis}>{this.state.text}</button>
+  }
+
+});
+
+module.exports = React.createClass({
+  'displayName': 'BlankPage.jsx',
 
   render: function () {
     var title = 'This page is under construction';
     var header = <Header title={title} />;
-    var likeButton = <button type="button" id="likeButton" onClick={this.likeThis}>{this.state.text}</button>;
 
     return <div className='Page'>
       {header}
-      {likeButton}
+      <LikeButton />
     </div>
   }
 });
