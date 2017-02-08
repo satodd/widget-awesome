17fcfccdd72db0cb2801e1a178715bfea4ca2794
satodd <saatodd@gmail.com>
2017-02-08 09:59:08 -0700

    Updated/fixed missing semi-colons, Added test explaination

--- test/unit.js
+++ test/unit.js
@@ -1,6 +1,11 @@
 var test = require('tape')
 
-test('fill in this', function (t) {
+test('single click of like button', function (t) {
   t.ok(true)
   t.end()
-})
+ })
+  /*
+  Not sure how to exactly write this test in this frame work but here is generally how I would do it:
+	First the like button has to be rendered in some way, check that the button is unliked. Then preform
+	a click function on the like button, save/check the value. Perform another click, check to make sure the 
+	value is unchanged. Can also check the like counter in the same way.
